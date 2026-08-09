ba6e241d44e82d37ac9afb904aca0980bfeef63a
Prakhar1808 <Prakhar1808@users.noreply.github.com>
2026-08-09 08:33:39 +0530

    fix: connect main and HUD

--- tutorial2D/main.gd
+++ tutorial2D/main.gd
@@ -14,11 +14,15 @@ func _process(delta: float) -> void:
 func game_over() -> void:
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	get_tree().call_group("mobs", "queue_free")
 
 func _on_mob_timer_timeout() -> void:
 	var mob = mob_scene.instantiate()
@@ -41,3 +45,4 @@ func _on_start_timer_timeout() -> void:
 
 func _on_score_timer_timeout() -> void:
 	score += 1
+	$HUD.update_score(score)
